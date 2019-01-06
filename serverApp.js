bfaf89ba39d33e56ce425b957b469ac4cb521174
elyabe <elyabe@outlook.com>
2019-01-06 14:22:43 -0200

    Realocação do server

--- serverApp.js
+++ serverApp.js
@@ -1,4 +1,4 @@
-const express = require('express')
+/*const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
@@ -9,3 +9,14 @@ express()
   // .get('/', (req, res) => res.render('pages/index'))
   .get('/', (req, res) => res.render('pages/home', { title: 'PPC Choice' } ) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+*/
+
+#!/usr/bin/nodejs
+var debug = require('debug')('ppc_choice');
+var app = require('../app');
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  debug('Express server listening on port ' + server.address().port);
+});
